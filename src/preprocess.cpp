101f1a8c0a5755f7fcd15535ff4801b59572d95b
sassy23 <sundaram.seivur@gmail.com>
2023-04-21 14:23:36 -0400

    Changes to FAST-LIVO sensor parameters to account for HILTI sensor params

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -56,6 +56,10 @@ void Preprocess::process(const sensor_msgs::PointCloud2::ConstPtr &msg, PointClo
   case VELO16:
     velodyne_handler(msg);
     break;
+
+  case hesaiXT:
+    hesaiXT_handler(msg);
+    break;
   
   default:
     printf("Error LiDAR Type");
@@ -159,6 +163,7 @@ void Preprocess::avia_handler(const livox_ros_driver::CustomMsg::ConstPtr &msg)
 
 void Preprocess::oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
+  ROS_INFO("I'm in OUST");
   pl_surf.clear();
   pl_corn.clear();
   pl_full.clear();
@@ -254,6 +259,107 @@ void Preprocess::oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
   // pub_func(pl_surf, pub_full, msg->header.stamp);
   // pub_func(pl_surf, pub_corn, msg->header.stamp);
 }
+
+
+void Preprocess::hesaiXT_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+  ROS_INFO("I'm in HESAI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  pl_surf.clear();
+  pl_corn.clear();
+  pl_full.clear();
+  // pcl::PointCloud<ouster_ros::Point> pl_orig;
+  pcl::fromROSMsg(*msg, pl_surf);
+  uint plsize = pl_surf.size();
+  pl_corn.reserve(plsize);
+  // pl_surf.reserve(plsize);
+  // if (feature_enabled)
+  // {
+  //   for (int i = 0; i < N_SCANS; i++)
+  //   {
+  //     pl_buff[i].clear();
+  //     pl_buff[i].reserve(plsize);
+  //   }
+
+  //   for (uint i = 0; i < plsize; i++)
+  //   {
+  //     double range = pl_orig.points[i].x * pl_orig.points[i].x + pl_orig.points[i].y * pl_orig.points[i].y + pl_orig.points[i].z * pl_orig.points[i].z;
+  //     if (range < blind) continue;
+  //     Eigen::Vector3d pt_vec;
+  //     PointType added_pt;
+  //     added_pt.x = pl_orig.points[i].x;
+  //     added_pt.y = pl_orig.points[i].y;
+  //     added_pt.z = pl_orig.points[i].z;
+  //     added_pt.intensity = pl_orig.points[i].intensity;
+  //     added_pt.normal_x = 0;
+  //     added_pt.normal_y = 0;
+  //     added_pt.normal_z = 0;
+  //     double yaw_angle = atan2(added_pt.y, added_pt.x) * 57.3;
+  //     if (yaw_angle >= 180.0)
+  //       yaw_angle -= 360.0;
+  //     if (yaw_angle <= -180.0)
+  //       yaw_angle += 360.0;
+
+  //     added_pt.curvature = pl_orig.points[i].t / 1e6;
+  //     if(pl_orig.points[i].ring < N_SCANS)
+  //     {
+  //       pl_buff[pl_orig.points[i].ring].push_back(added_pt);
+  //     }
+  //   }
+
+  //   for (int j = 0; j < N_SCANS; j++)
+  //   {
+  //     PointCloudXYZI &pl = pl_buff[j];
+  //     uint linesize = pl.size();
+  //     vector<orgtype> &types = typess[j];
+  //     types.clear();
+  //     types.resize(linesize);
+  //     linesize--;
+  //     for (uint i = 0; i < linesize; i++)
+  //     {
+  //       types[i].range = sqrt(pl[i].x * pl[i].x + pl[i].y * pl[i].y);
+  //       vx = pl[i].x - pl[i + 1].x;
+  //       vy = pl[i].y - pl[i + 1].y;
+  //       vz = pl[i].z - pl[i + 1].z;
+  //       types[i].dista = vx * vx + vy * vy + vz * vz;
+  //     }
+  //     types[linesize].range = sqrt(pl[linesize].x * pl[linesize].x + pl[linesize].y * pl[linesize].y);
+  //     give_feature(pl, types);
+  //   }
+  // }
+  // else
+  // {
+  //   double time_stamp = msg->header.stamp.toSec();
+  //   // cout << "===================================" << endl;
+  //   // printf("Pt size = %d, N_SCANS = %d\r\n", plsize, N_SCANS);
+  //   for (int i = 0; i < pl_orig.points.size(); i++)
+  //   {
+  //     if (i % point_filter_num != 0) continue;
+
+  //     double range = pl_orig.points[i].x * pl_orig.points[i].x + pl_orig.points[i].y * pl_orig.points[i].y + pl_orig.points[i].z * pl_orig.points[i].z;
+      
+  //     if (range < blind) continue;
+      
+  //     Eigen::Vector3d pt_vec;
+  //     PointType added_pt;
+  //     added_pt.x = pl_orig.points[i].x;
+  //     added_pt.y = pl_orig.points[i].y;
+  //     added_pt.z = pl_orig.points[i].z;
+  //     added_pt.intensity = pl_orig.points[i].intensity;
+  //     added_pt.normal_x = 0;
+  //     added_pt.normal_y = 0;
+  //     added_pt.normal_z = 0;
+
+
+  //     added_pt.curvature = pl_orig.points[i].t / 1e6;
+
+  //     // cout<<"added_pt.curvature: "<<added_pt.curvature<<endl;
+  //     pl_surf.points.push_back(added_pt);
+  //   }
+  // }
+  // pub_func(pl_surf, pub_full, msg->header.stamp);
+  // pub_func(pl_surf, pub_corn, msg->header.stamp);
+}
+
 
 void Preprocess::velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
