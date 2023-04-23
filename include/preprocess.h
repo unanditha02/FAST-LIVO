e96e2a0a0ec749328faf52a29063b45c7087b334
Anthony Kyu <akyu@andrew.cmu.edu>
2023-04-23 19:15:09 -0400

    tried more to get hesai working

--- include/preprocess.h
+++ include/preprocess.h
@@ -82,7 +82,7 @@ namespace hesai_ros {
     PCL_ADD_POINT4D   //添加pcl里xyz
     float intensity;
     double timestamp;
-    uint16_t ring;                   ///< laser ring number
+    std::uint16_t ring;                   ///< laser ring number
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW  // make sure our new allocators are aligned,确保定义新类型点云内存与SSE对齐
   } EIGEN_ALIGN16;                   // 强制SSE填充以正确对齐内存
 
@@ -92,7 +92,7 @@ namespace hesai_ros {
 POINT_CLOUD_REGISTER_POINT_STRUCT(
     hesai_ros::PointXYZIT,
     (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
-        double, timestamp, timestamp)(std::uint32_t, ring, ring))
+        double, timestamp, timestamp)(std::uint16_t, ring, ring))
 typedef hesai_ros::PointXYZIT PPoint;
 typedef pcl::PointCloud<PPoint> PPointCloud;
 
